5dd8c93a2c5ac3e89c8f333031a5b80e40b2ff88
Prajakta Purohit <prajakta@chef.io>
2020-07-10 12:20:06 -0700

    update ohai
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -9,7 +9,7 @@ override :rubygems, version: "3.0.3"
 override :bundler, version: "1.17.2"  # pin to avoid double bundle error
 override :'omnibus-ctl', version: "master"
 override :chef, version: "v15.12.22"
-override :ohai, version: "v15.7.4"
+override :ohai, version: "v15.12.0"
 override :ruby, version: "2.6.5"
 override :perl, version: "5.18.1"
 
